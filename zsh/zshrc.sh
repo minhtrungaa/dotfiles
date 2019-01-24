1fe6bc44c043f5e2c8438ba952582bad41a87c33
Trung Nguyen <trung.nguyenhoangminh@stdio.vn>
2019-01-24 13:02:32 +0700

    + Add zsh config script + Add vim config + Add tmux.conf but not have anything init - change deploy to deploy.sh

---
+++ zsh/zshrc.sh
@@ -0,0 +1,25 @@
+export ZSH=$HOME/.oh-my-zsh
+export JAVA_HOME="/media/trung/OS/dev-tools/java-linux/jdk1.8.0_192"
+export ANDROID_HOME="/media/trung/OS/dev-tools/android-linux/sdk"
+export PATH=$PATH:$JAVA_HOME
+export PATH=$PATH:$JAVA_HOME/bin
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export PATH=$HOME/bin:usr/local/bin:$PATH
+export PATH=$HOME/.local/share/umake/bin:$PATH
+export TERMINAL="terminator"
+export VISUAL=vim
+export EDITOR="$VISUAL"
+
+ZSH_THEME="spaceship"
+
+plugins=(
+  git
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
+
+source $ZSH/oh-my-zsh.sh
+source ~/dotfiles/zsh/keybindings.sh
